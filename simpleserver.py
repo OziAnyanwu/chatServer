fa745510e020f7887c1201479432d853bbda36b4
Ozioma-Jesus Anyanwu <ozijesus@stanford.edu>
2018-06-25 01:14:13 -0700

    testing

--- simpleserver.py
+++ simpleserver.py
@@ -15,13 +15,15 @@ class Server:
         """Sockets must be bound and listening in order to accept connections"""
 
 
-
+    def t2Handler(self,connexion):
+        while True:
+            connexion.send(bytes(input(""),'utf-8'))
+        
     def tHandler(self, connexion, cl_addr):
             #global connections
             while True:
                 data = connexion.recv(1024)
                 print(str(data,'utf-8'))
-                connexion.send(bytes(input(""),'utf-8'))
                 #for c in self.connections:
                     #c.send(bytes(data))
                 if not data:
@@ -35,6 +37,9 @@ class Server:
                 cThread = threading.Thread(target=self.tHandler, args=(connexion,cl_addr))
                 cThread.daemon = True #lets program exit even if a thread is running
                 cThread.start()
+                i2Thread = threading.Thread(target=self.t2Handler, args=(connexion))
+                i2Thread.daemon = True
+                i2Thread.start()
                 self.connections.append(connexion)
                 print(str(cl_addr[0]) + ':' + str(cl_addr[1]), "connnected")
 class Client:
